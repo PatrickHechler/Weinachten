e3be0ce5391a575512744f10dd25cb214d483bac
Patrick <Patrick@Coruscant.Speedport_W_724V_01011603_07_002>
2020-11-22 20:48:28 +0100

    added new arg

--- BWINF-39/A5Wichteln/src/de/hechler/patrick/haupt/Haupt.java
+++ BWINF-39/A5Wichteln/src/de/hechler/patrick/haupt/Haupt.java
@@ -71,6 +71,11 @@ public class Haupt {
 				break;
 			case "-f":
 			case "-file":
+				if (console) {
+					System.err.println("wrong args, can't read from console and file!");
+					help(System.err);
+					System.exit(1);
+				}
 				if (file != null) {
 					System.err.println("wrong args, can't read from two Files!");
 					help(System.err);
@@ -79,6 +84,39 @@ public class Haupt {
 				i ++ ;
 				if (args.length < i) {
 					System.err.println("wrong args, there are not enugh elements in the Array!");
+					help(System.err);
+					System.exit(1);
+				}
+				file = new File(args[i]);
+				break;
+			case "-pf":
+			case "-potfile":
+				if (args.length == i + 1) {
+					if (file != null) {
+						System.err.println("wrong args, can't read from console and file!");
+						help(System.err);
+						System.exit(1);
+					}
+					console = true;
+					break;
+				}
+				if (args[i + 1].startsWith("-")) {
+					if (file != null) {
+						System.err.println("wrong args, can't read from console and file!");
+						help(System.err);
+						System.exit(1);
+					}
+					console = true;
+					break;
+				}
+				i++;
+				if (file != null) {
+					System.err.println("wrong args, can't read from two files!");
+					help(System.err);
+					System.exit(1);
+				}
+				if (console) {
+					System.err.println("wrong args, can't read from console and file!");
 					help(System.err);
 					System.exit(1);
 				}
